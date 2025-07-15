490c7c554a125a9a92cfa0ceb93e2916908a7051
Tavo <tavo@Tavos-MacBook-Pro.local>
2025-07-15 15:35:53 -0700

    update definitions

--- definitions.py
+++ definitions.py
@@ -1,15 +1,3 @@
-# full sets of characters
-UPPERCASE = 'ABCDEFGHJKLMNPQRSTUVWXYZ'
-LOWERCASE = 'abcdefghijkmnopqrstuvwxyz'
-DIGITS    = '23456789'
-SYMBOLS   = '!@#$%^&*'
-
-# unambiguous alphanumerics
-U_UPPERCASE = 'ABCDEFGHJKLMNPQRSTUVWXYZ'     # removed I and O
-U_LOWERCASE = 'abcdefghijkmnopqrstuvwxyz'    # removed l
-U_DIGITS    = '23456789'                     # removed 0 and 1
-
-
 CHARSETS = {
     'uppercase': 'ABCDEFGHJKLMNPQRSTUVWXYZ',
     'lowercase': 'abcdefghijkmnopqrstuvwxyz',
